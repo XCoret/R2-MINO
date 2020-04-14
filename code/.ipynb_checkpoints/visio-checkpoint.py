c81ec6264d31afb84fae7e3e7fc0a6ef68f2c3eb
XaviCoret <xavier.coret@e-campus.uab.cat>
2020-04-14 16:45:27 +0200

    notebook començat

---
+++ code/.ipynb_checkpoints/visio-checkpoint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+Computa els canvis registrats en el taulell de joc i ho passa al Mòdul Control.
+'''
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+print('Modul Visio Carregat!')
+def llegirImatge(path):
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+
+    plt.imshow(gray,'gray')
+    plt.show()
+
