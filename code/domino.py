ee6c9c4e19faecb6db84a48e3dcf1f4c31693df0
MarcCallado <marc.callado@e-campus.uab.cat>
2020-04-26 16:59:13 +0200

    Algorisme de Domino i Control equemàtic

--- code/domino.py
+++ code/domino.py
@@ -27,3 +27,37 @@ def func():
     for f in fitxes:
         print(str(f.vA)+' '+str(f.vB))
     print(len(fitxes))
+
+#Contra opció
+class playerAlgorithm:
+
+    def __init__(self, playerType):
+        print ("Constructor")
+        self.__playerType = playerType
+
+    def humanLoop(self):
+        #wait: Nova peça apareix, no queden peces al pou per primera vegada i el jugador no pot tirar (revisar des de visió)
+
+    def robotLoop(self):
+        playing = true
+
+        while(playing):
+            #Hi ha fitxa disponible que encaixi en valor amb algun dels extrems?
+            if(validTocken):
+                #Col·locarla
+                playing = false
+                return true
+            else:
+                if(moreTockens):
+                    #Agafar fitxa del pou
+                else:
+                    #Passar torn
+                    playing = false
+                    return false
+
+
+    def playTurn(self):
+        if(self.__playerType == "h"):
+            humanLoop
+        elif(self.__playerType == "r"):
+            robotLoop
